9298d52f514a5e1d4cfdb124e312bfdeaae4c348
lzgabel <lz19960321lz@gmail.com>
2023-11-14 19:36:21 +0800

    chore: disable collaboration, excalidraw plus

--- excalidraw-app/components/AppMainMenu.tsx
+++ excalidraw-app/components/AppMainMenu.tsx
@@ -14,27 +14,27 @@ export const AppMainMenu: React.FC<{
       <MainMenu.DefaultItems.SaveToActiveFile />
       <MainMenu.DefaultItems.Export />
       <MainMenu.DefaultItems.SaveAsImage />
-      {props.isCollabEnabled && (
-        <MainMenu.DefaultItems.LiveCollaborationTrigger
-          isCollaborating={props.isCollaborating}
-          onSelect={() => props.setCollabDialogShown(true)}
-        />
-      )}
+      {/*{props.isCollabEnabled && (*/}
+      {/*  <MainMenu.DefaultItems.LiveCollaborationTrigger*/}
+      {/*    isCollaborating={props.isCollaborating}*/}
+      {/*    onSelect={() => props.setCollabDialogShown(true)}*/}
+      {/*  />*/}
+      {/*)}*/}
 
       <MainMenu.DefaultItems.Help />
       <MainMenu.DefaultItems.ClearCanvas />
       <MainMenu.Separator />
-      <MainMenu.ItemLink
-        icon={PlusPromoIcon}
-        href={`${
-          import.meta.env.VITE_APP_PLUS_LP
-        }/plus?utm_source=excalidraw&utm_medium=app&utm_content=hamburger`}
-        className="ExcalidrawPlus"
-      >
-        Excalidraw+
-      </MainMenu.ItemLink>
-      <MainMenu.DefaultItems.Socials />
-      <MainMenu.Separator />
+      {/*<MainMenu.ItemLink*/}
+      {/*  icon={PlusPromoIcon}*/}
+      {/*  href={`${*/}
+      {/*    import.meta.env.VITE_APP_PLUS_LP*/}
+      {/*  }/plus?utm_source=excalidraw&utm_medium=app&utm_content=hamburger`}*/}
+      {/*  className="ExcalidrawPlus"*/}
+      {/*>*/}
+      {/*  Excalidraw+*/}
+      {/*</MainMenu.ItemLink>*/}
+      {/*<MainMenu.DefaultItems.Socials />*/}
+      {/*<MainMenu.Separator />*/}
       <MainMenu.DefaultItems.ToggleTheme />
       <MainMenu.ItemCustom>
         <LanguageList style={{ width: "100%" }} />
